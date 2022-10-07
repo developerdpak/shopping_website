ce776a6967554f312bc3053febd6d1780b66e6f2
developerdpak <hope.deepak2@gmail.com>
2022-10-07 09:35:14 -0500

    "iMacFriday

--- shopping_website/app/urls.py
+++ shopping_website/app/urls.py
@@ -1,6 +1,15 @@
 from django.urls import path, include
 from . import views
+from django.contrib.auth import views as auth_views
+from . forms import LoginForm
+
+app_name = 'app'
 
 urlpatterns = [
-    path('', views.index, name='index')
+    # path('', views.index, name='index'),
+    path('home/', views.ProductView.as_view(), name='home'),
+    path('product/<int:pk>', views.ProductDetailView.as_view(), name='product-detail'),
+    path('accounts/login/', auth_views.LoginView.as_view(template_name='app/login.html', authentication_form=LoginForm), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='/'), name='logout'),
+    path('register/', views.CustomerRegistration.as_view(), name='customerregistration')
 ]
--- shopping_website/app/views.py
+++ shopping_website/app/views.py
@@ -1,5 +1,31 @@
 from django.shortcuts import render
+from django.views import View
+from .forms import CustomerRegistrationForm
+from . models import Product
+
 
 # Create your views here.
 def index(request):
     return render(request, 'app/index.html')
+
+
+class ProductView(View):
+    def get(self, request):
+        laptops = Product.objects.filter(category='L')
+        television = Product.objects.filter(category='TV')
+        mobiles = Product.objects.filter(category='M')
+        washing_machine = Product.objects.filter(category='WM')
+        return render(request, 'app/home.html', {'laptops': laptops, 'television': television, 'mobiles': mobiles, 'washing_machine': washing_machine})
+
+
+class CustomerRegistration(View):
+    def get(self, request):
+        form = CustomerRegistrationForm()
+        return render(request, 'app/customerregistration.html', {'form':form})
+
+class ProductDetailView(View):
+    def get(self, request, pk):
+        product = Product.objects.get(pk=pk)
+        print(product.id)
+        return render(request, 'app/productdetail.html', {'product': product})
+
